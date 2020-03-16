00ffad4e2e737b0e5052ba14f1f20987a650416a
BuildTools <unconfigured@null.spigotmc.org>
2020-03-16 13:28:47 +0000

    Firebase auth

--- app/src/main/java/com/example/week2ex1/LoggedIn.java
+++ app/src/main/java/com/example/week2ex1/LoggedIn.java
@@ -24,64 +24,14 @@ import java.util.Collections;
 
 public class LoggedIn extends AppCompatActivity {
 
-    int numberOfPosts = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_logged_in);
-        display();
     }
     @Override
     protected void onResume(){
         super.onResume();
-        display();
-    }
-    public void post(View view){
-        Intent postIntent = new Intent(this, PostActivity.class);
-        startActivity(postIntent);
-    }
-
-    public void refresh(View V){
-        display();
-    }
-    public void display(){
-        FirebaseFirestore db = FirebaseFirestore.getInstance();
-        final ArrayList<String> postList = new ArrayList<>();
-        //Get database posts
-        db.collection("posts").orderBy("time")
-                .get()
-                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>(){
-                    @Override
-                    public void onComplete(@NonNull Task<QuerySnapshot> task){
-                        if(task.isSuccessful()){
-                            for(QueryDocumentSnapshot document : task.getResult()){
-                                numberOfPosts++;
-                                //System.out.println(document.getId());
-                                //System.out.println(document.getData());
-                                postList.add("\n" + document.get("username") + "\n" + document.get("time") + "\n" + document.get("post") + "\n");
-                                //TextView info = (TextView) findViewById(R.id.info);
-                                //info.append("\n" + document.get("username") + "\n" + document.get("post") + "\n");
-                            }
-                        }else{
-                            Log.w("tag", "Error getting document", task.getException());
-                        }
-
-                        TextView info = (TextView) findViewById(R.id.info);
-
-                        String concat = "";
-                        Collections.reverse(postList);
-                        //System.out.println("First call");
-                        for(int i = 0; i < postList.size(); i++){
-                            if(i > 4)
-                                break;
-
-                            concat += postList.get(i);
-                            //System.out.println(postList.get(i));
-
-                        }
-                        info.setText(concat);
-                    }
-                });
     }
 }
